20080de9b14a32d28144aa35dbdbed4513fbcb12
José Cabral <cabraljose1210@gmail.com>
2023-11-10 11:32:15 -0300

    Added routes file and map to facilitate tasks

---
+++ src/config.py
@@ -0,0 +1,10 @@
+map_barrios = './utils/maps/barrios.shp'
+
+path_df_LV = '../Data/Fuentes/LV/df_LV.csv'
+path_df_ML = '../Data/Fuentes/ML/df_ML.csv'
+path_df_ZP = '../Data/Fuentes/ZP/df_ZP.csv'
+
+path_reports = '../Data/Reportes/'
+path_unify = '../Data/Unificadas/'
+
+anteriores = '../Data/Anteriores/'
