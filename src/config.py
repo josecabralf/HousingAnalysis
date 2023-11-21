4a373b2a9ccae7624a18ed6ee588532f5d2c9c3e
José Cabral <cabraljose1210@gmail.com>
2023-11-21 12:39:10 -0300

    Nuevos test y variables consideradas

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
 map_barrios = './utils/maps/Barrios/barrios.shp'
+map_comisarias = './utils/maps/Comisarias/comisarias.csv'
+map_ctros_salud = './utils/maps/CS/centros_salud.geojson'
+map_escuelas_muni = './utils/maps/EM/escuelas_muni.geojson'
+map_transporte_urb = './utils/maps/TU/transporte_urb.csv'
+
 
 path_df_LV = '../Data/Fuentes/LV/df_LV.csv'
 path_df_ML = '../Data/Fuentes/ML/df_ML.csv'
@@ -8,3 +13,5 @@ path_reports = '../Data/Reportes/'
 path_unify = '../Data/Unificadas/'
 
 anteriores = '../Data/Anteriores/'
+actuales = '../Data/actuales.csv'
+train = '../Data/train.csv'
