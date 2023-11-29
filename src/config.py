5d932a34357773740c4b6dc3cee011bd55a4001c
José Cabral <cabraljose1210@gmail.com>
2023-11-29 15:02:12 -0300

    AdaBoostRegressor con DecisionTreeRegressor para correcciones

--- src/config.py
+++ src/config.py
@@ -15,8 +15,14 @@ path_unify = '../Data/Unificadas/'
 anteriores = '../Data/Anteriores/'
 actuales = '../Data/actuales.csv'
 train = '../Data/train.csv'
+formated_train = '../Data/formated_train.csv'
 
 all = "../Data/all.csv"
 
 kmeans_model = "./utils/models/kmeans.joblib"
+kmeans_scaler = './utils/models/scalers/kmeans_scaler.joblib'
+
 ensamble_precio_model = "./utils/models/ensemble_model.joblib"
+ensamble_precio_scaler = './utils/models/scalers/ensamble_scaler.joblib'
+
+adaboost_corrector_model = "./utils/models/adaboost_corrector.joblib"
